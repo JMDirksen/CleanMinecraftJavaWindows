ab9a1d429031bcc329c3231c069cba828944b522
JMDirksen <jeftadirksen@gmail.com>
2023-05-20 09:42:19 +0200

    stats

---
+++ stats.cmd
@@ -0,0 +1,45 @@
+@echo off
+
+:: Load defaults
+set last=0
+set max=0
+set dAvg=0
+set mAvg=0
+
+:: Load stored data
+if exist stats.db for /f %%a in (stats.db) do set %%a
+
+:: Get local ip address
+for /f "tokens=2 delims=:" %%a in ('ipconfig ^| find "IPv4"') do set ip=%%a
+set ip=%ip:~1%
+
+:: Get Minecraft server connections
+for /f %%a in ('netstat -n ^| find "%ip%:25565" ^| find /c "ESTABLISHED"') do set current=%%a
+
+:: Calculate stats
+set last=%current%
+if %current% gtr %max% set max=%current%
+set /a dAvg=( %dAvg% * 287 + %current% * 1000000 ) / 288
+set /a mAvg=( %mAvg% * 8639 + %current% * 1000000 ) / 8640
+
+:: Save data
+echo last=%last% >stats.db
+echo max=%max% >>stats.db
+echo dAvg=%dAvg% >>stats.db
+echo mAvg=%mAvg% >>stats.db
+
+:: Output HTML
+set /a daily=%dAvg% / 100000
+set /a monthly=%mAvg% / 100000
+
+echo ^<html^>^<head^>^<title^>Stats (%current%)^</title^>^<meta http-equiv=refresh content=60^>^</head^>^<body^>^<p^> >stats.html
+echo Current: %current% ^<br^> >>stats.html
+echo Max: %max% ^<br^> >>stats.html
+echo Daily average: %daily%/10 ^<br^> >>stats.html
+echo Monthly average: %monthly%/10 ^<br^> >>stats.html
+echo ^</p^>^</body^>^</html^> >>stats.html
+
+:: Copy to webserver
+copy /y stats.html C:\Data\www\cleanmc.com\stats\index.html
+
+exit /b
